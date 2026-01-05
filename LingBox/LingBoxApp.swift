47061e4542860b992349a428028e68a54b36caeb
Leo <979288551@qq.com>
2026-01-05 17:13:48 +0800

    Initial Commit

---
+++ LingBox/LingBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingBoxApp.swift
+//  LingBox
+//
+//  Created by 超爹 on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct LingBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LingBoxTests/LingBoxTests.swift
@@ -0,0 +1,16 @@
+//
+//  LingBoxTests.swift
+//  LingBoxTests
+//
+//  Created by 超爹 on 2026/1/5.
+//
+
+import Testing
+
+struct LingBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
